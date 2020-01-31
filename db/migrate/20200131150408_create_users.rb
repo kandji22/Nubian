0aad411abf2fb6a1427b8f82407c48874cc75306
kandji <kandji.k66@gmail.com>
2020-01-31 16:41:07 +0100

    install gem

---
+++ db/migrate/20200131150408_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :mail
+      t.string :role
+      t.string :profil_img
+
+      t.timestamps
+    end
+  end
+end
