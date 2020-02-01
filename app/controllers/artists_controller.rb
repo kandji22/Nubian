10c4e8afa902a434176fb49e7a4ec8a736fd3ca2
kandji <kandji.k66@gmail.com>
2020-02-01 08:40:00 +0100

    add edit new artist

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: %i[show edit update destroy]
+
+  def new
+    @artist = Artist.new
+   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    respond_to do |format|
+      if @artist.save
+        # In this format call, the flash message is being passed directly to
+        # redirect_to().  It's a caonvenient way of setting a flash notice or
+        # alert without referencing the flash Hash explicitly.
+        format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @artist.update(artist_params)
+        # In this format call, the flash message is being passed directly to
+        # redirect_to().  It's a caonvenient way of setting a flash notice or
+        # alert without referencing the flash Hash explicitly.
+        format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :photo, :bibliographie, :url_facebook, :url_instagram)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :users
   resources :sessions, only: %i[new create destroy]
+  resources :artists
 end
