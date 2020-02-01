50e6ea39889d6904911355063c5b3b0ee8420a51
kandji <kandji.k66@gmail.com>
2020-02-01 07:26:38 +0100

    config session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(mail: params[:mail].downcase)
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:alert] = 'password or email failed'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+   end
 end
