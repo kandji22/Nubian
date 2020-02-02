f08c90f50523e044e55eee34d8126ebb00fdb2b3
kandji <kandji.k66@gmail.com>
2020-02-02 19:13:22 +0100

    add modified and delete by admin artists

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,2 +1,5 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
+let Artist = {}
+Artist.RemoveArtist = function(idartist) {
+  document.getElementById("artist-"+idartist).parentElement.remove()
+};
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class AdminController < ApplicationController
+  def artists
+    @artists = Artist.all
+  end
 end
